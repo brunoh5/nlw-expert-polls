2af983019bc9caaeb7d0f07a39a2405db66f721b
Bruno Henrique <brunohenriquesantos272@gmail.com>
2024-02-09 13:37:39 -0300

    feat: terminando a aplicação

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,28 +1,26 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import { z } from 'zod'
+import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
+
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
 
-const prisma = new PrismaClient()
-
-app.post('/polls', async (request, reply) => {
-	const createPollBody = z.object({
-		title: z.string(),
-	})
-
-	const { title } = createPollBody.parse(request.body)
-
-	const poll = await prisma.poll.create({
-		data: {
-			title,
-		},
-	})
-
-	return reply.status(201).send({
-		pollId: poll.id,
-	})
+app.register(cookie, {
+	secret: 'signInCookieGeneratedByThisAPI',
+	hook: 'onRequest',
 })
+
+app.register(websocket)
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
+
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
 	console.info('HTTP Server running')
